b20d8e0dd9461d608d0474bb9e19a4e7a6d27e9c
Olena Zozulia <viktorzozylia@gmail.com>
2023-03-04 20:16:48 +0200

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createCardsGallery(galleryItems));
+galleryEl.addEventListener("click", handleClickOnCards);
+
+function createCardsGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    }).join('');
+};
+
+function handleClickOnCards(e) {
+  e.preventDefault();
+
+  const modal = basicLightbox.create(`
+   <div class="modal">
+    <img
+      class="gallery__image"
+      src="${e.target.dataset.source}"/>
+   </div>
+`)
+  console.log(modal.show());
+
+  if (modal.show()) {
+    document.addEventListener('keydown', e => {
+      if (e.key === 'Escape') {
+       modal.close(() => console.log('lightbox close'))
+    }
+    });
+  }
+  
+  if (e.target.NodeName === 'IMG') {
+    return modal;
+  } else {
+    return;
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function createCardsGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    }).join('');
+};
+
+galleryEl.insertAdjacentHTML('beforeend', createCardsGallery(galleryItems));
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
